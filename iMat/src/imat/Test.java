a5f794f7ea728d68c296431e2be194113af1bbf0
unknown <lassepalten@gmail.com>
2015-02-20 14:29:56 +0100

    kodbas

---
+++ iMat/src/imat/Test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imat;
+
+import java.io.File;
+import javax.swing.*;
+import se.chalmers.ait.dat215.project.*;
+import se.chalmers.ait.dat215.project.util.*;
+
+/**
+ *
+ * @author Lasse
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+        IMatDataHandler dh = IMatDataHandler.getInstance();
+        dh.getShoppingCart().clear();
+        dh.shutDown();
+        
+        
+    }
+}
