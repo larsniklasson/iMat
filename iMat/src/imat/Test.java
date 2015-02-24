07cca1e6d46a4833497e378d935fb58a48030d32
unknown <lassepalten@gmail.com>
2015-02-24 23:39:23 +0100

    popup till listor, på produktvyn

--- iMat/src/imat/Test.java
+++ iMat/src/imat/Test.java
@@ -23,109 +23,13 @@ import se.chalmers.ait.dat215.project.util.*;
  */
 public class Test {
     
-    IMatDataHandler dh;
-    
-    public Test(){
-        dh = IMatDataHandler.getInstance();
-        ShoppingCart sc = dh.getShoppingCart();
-        sc.addProduct(dh.getProduct(1), 2);
-        sc.addProduct(dh.getProduct(7), 5);
-        sc.addProduct(dh.getProduct(8), 9);
-        
-        saveShoppingCartAsList("lista 1");
-        
-        /*
-        List<Order> orders = dh.getOrders();
-        orders.sort(new Comparator<Order>() {
-
-            @Override
-            public int compare(Order o1, Order o2) {
-                return o1.getOrderNumber() - o2.getOrderNumber();
-            }
-        });
-        
-        for(Order o : orders){
-            
-            System.out.println(o.getOrderNumber());
-        }
-        */
-        /*ShoppingCart sc = dh.getShoppingCart();
-        sc.addProduct(dh.getProduct(2));
-        sc.addProduct(dh.getProduct(1));
-        sc.addProduct(dh.getProduct(5));
-        sc.addProduct(dh.getProduct(6));
-        dh.placeOrder();
-        dh.shutDown();
-        
-        Order o = dh.getOrders().get(dh.getOrders().size()-1);
-        
-        try{
-            File f = new File(dh.imatDirectory() + "\\orders\\order_" + o.getOrderNumber() + ".txt");
-            
-            if(f.delete()){
-                System.out.println("file was deleted");
-            } else {
-                System.out.println("file was not deleted");
-            }
-            
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        
-        o.setOrderNumber(-4);
-        dh.shutDown();*/
-        
-        
-        
-        
-        
-        
-    }
-    
-    public void saveShoppingCartAsList(String name){
-        String path = dh.imatDirectory() + "\\inköpslistor";
-        File dir = new File(path);
-        if(!dir.exists()){
-            dir.mkdirs();
-        }
-        
-        File f = new File(path + "\\" + name + ".txt");
-        
-        
-        
-        
-        if(!f.exists()) {
-            try {
-                f.createNewFile();
-            } catch (IOException ex) {
-                Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        
-        PrintWriter pw = null;
-        try {
-            pw = new PrintWriter(f);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        for(ShoppingItem si : dh.getShoppingCart().getItems()){
-            pw.print(si.getProduct().getProductId() + ":" + si.getAmount() + ";");
-        }
-        
-        
-        pw.close();
-    }
     
     
     
     public static void main(String[] args) {
-        IMatDataHandler dh = IMatDataHandler.getInstance();
-        //System.out.println(dh.imatDirectory());
-        File f = new File(dh.imatDirectory() + "\\inköpslistor");
-        
-        f.mkdirs();
-        
+        for(String s: Utils.getListor()){
+            System.out.println(s);
+        }
         
     }
 }
--- iMat/src/imat/Utils.java
+++ iMat/src/imat/Utils.java
@@ -9,10 +9,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
 import se.chalmers.ait.dat215.project.ShoppingItem;
 
 /**
@@ -121,4 +124,162 @@ public class Utils {
         return true;
 
     }
+    
+    public static void addProductToList(Product p, String listName){
+        File folder = new File(IMatDataHandler.getInstance().imatDirectory() + "/inköpslistor");
+        
+        File[] listOfFiles = folder.listFiles();
+        
+        Scanner sc = null;
+        for(File f : listOfFiles){
+            try {
+                sc = new Scanner(f);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(ListorPanel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            try{
+                if(listName.equals(sc.nextLine().split(";")[0])){
+                    System.out.println("hej");
+                    sc.close();
+                    sc = new Scanner(f);
+                    
+                    String newString = "";
+                    
+                    String[] arr = sc.nextLine().split(";");
+                    newString += arr[0] + ";";
+                    
+                    boolean wasInList = false;
+                    
+                    int counter = 0;
+                    for(String s : arr){
+                        counter ++;
+                        if(counter == 1){
+                            continue;
+                        }
+                        
+                        String[] x = s.split(":");
+                        
+                        if(Integer.parseInt(x[0]) == p.getProductId()){
+                            wasInList = true;
+                            if(p.getUnit().equals("kr/kg")){
+                                newString += x[0] + ":" + (Double.parseDouble(x[1]) + 0.2) + ";";
+                            } else {
+                                newString += x[0] + ":" + (Double.parseDouble(x[1]) + 1.0) + ";";
+                            }
+                        } else {
+                            newString += s + ";";
+                        }
+                        System.out.println("newstring = " + newString);
+                        
+                        
+                    }
+                    
+                    if(!wasInList){
+                        if(p.getUnit().equals("kr/kg")){
+                            newString += p.getProductId() + ":" + 0.2 + ";";
+                        } else {
+                            newString += p.getProductId() + ":" + 1.0 + ";";
+                        }
+                    }
+                    
+                    sc.close();
+                    PrintWriter pw = null;
+                    try {
+                        pw = new PrintWriter(f);
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(ListorPanel.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    
+                    pw.print(newString);
+                    pw.close();
+                    
+                    
+                    
+                    
+                    
+                    
+                } else {
+                    sc.close();
+                    continue;
+                }
+            } catch (Exception e){
+                sc.close();
+                continue;
+            }
+        }
+    }
+    
+    public static List<String> getListor(){
+        List<String> list = new ArrayList<String>();
+        
+        File folder = new File(IMatDataHandler.getInstance().imatDirectory() + "/inköpslistor");
+        
+        File[] listOfFiles = folder.listFiles();
+        Scanner sc = null;
+        for(File f: listOfFiles){
+            try {
+                sc = new Scanner(f);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            list.add(sc.nextLine().split(";")[0]);
+            sc.close();
+        }
+        
+        
+        return list;
+    }
+    
+    
+    public static void createEmptyList(String name){
+        IMatDataHandler dh = IMatDataHandler.getInstance();
+        
+        String path = dh.imatDirectory() + "/inköpslistor/";
+        
+        File f;
+        int counter = 1;
+        while(true){
+            f = new File(path + counter + ".txt");
+            if(!f.exists()){
+                try {
+                    f.createNewFile();
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                break;
+            }
+            counter ++;
+        }
+        
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(f);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        if(isNameOK(name)){
+            pw.print(name + ";");
+        } else {
+            counter = 2;
+            while(true){
+                
+                if(isNameOK(name + " (" + counter + ")")){
+                    pw.print(name + " (" + counter + ")" + ";");
+                    break;
+                }
+                counter ++;
+            }
+        }
+        pw.close();
+        
+        
+        
+    }
+    
 }
