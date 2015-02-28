1a1090565567ac5efe12b7d76c13025e3aab79f1
unknown <lassepalten@gmail.com>
2015-02-28 12:56:42 +0100

    menyer

--- iMat/src/imat/IMatView.java
+++ iMat/src/imat/IMatView.java
@@ -126,6 +126,7 @@ public class IMatView extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         mainPanel = new javax.swing.JPanel();
         topPanel = new javax.swing.JPanel();
@@ -137,11 +138,16 @@ public class IMatView extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         loginButton = new javax.swing.JLabel();
         leftPanel = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jTree2 = new javax.swing.JTree();
+        jPanel5 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTree1 = new javax.swing.JTree();
-        jScrollPane3 = new javax.swing.JScrollPane();
-        jTree2 = new javax.swing.JTree();
-        jSeparator1 = new javax.swing.JSeparator();
         rightPanel = new javax.swing.JPanel();
         kundvagnPanel = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
@@ -322,6 +328,89 @@ public class IMatView extends javax.swing.JFrame {
         mainPanel.add(topPanel, java.awt.BorderLayout.PAGE_START);
 
         leftPanel.setBackground(new java.awt.Color(236, 255, 235));
+        leftPanel.setLayout(new javax.swing.BoxLayout(leftPanel, javax.swing.BoxLayout.Y_AXIS));
+
+        jPanel4.setBackground(new java.awt.Color(235, 255, 235));
+        jPanel4.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel7.setText("ANVÄNDARE");
+        jPanel4.add(jLabel7);
+
+        leftPanel.add(jPanel4);
+
+        jPanel2.setBackground(new java.awt.Color(235, 255, 235));
+        jPanel2.setLayout(new java.awt.BorderLayout());
+
+        jTree2.setRootVisible(false);
+
+        jTree2.setToggleClickCount(1);
+
+        for(MouseListener ml : jTree2.getMouseListeners()){
+
+            jTree2.removeMouseListener(ml);
+        }
+        jTree2.setBackground(new java.awt.Color(236, 255, 235));
+        jTree2.setFont(new java.awt.Font("Segoe UI Semibold", 0, 14)); // NOI18N
+        javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("root");
+        javax.swing.tree.DefaultMutableTreeNode treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Favoriter");
+        treeNode1.add(treeNode2);
+        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Inköpslistor");
+        treeNode1.add(treeNode2);
+        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Färdiga Kassar");
+        javax.swing.tree.DefaultMutableTreeNode treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Recept1");
+        treeNode2.add(treeNode3);
+        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Recept2");
+        treeNode2.add(treeNode3);
+        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Recept3");
+        treeNode2.add(treeNode3);
+        treeNode1.add(treeNode2);
+        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Dagens Erbjudande");
+        treeNode1.add(treeNode2);
+        jTree2.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
+        jTree2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jTree2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jTree2MousePressed(evt);
+            }
+        });
+        jTree2.addTreeSelectionListener(new javax.swing.event.TreeSelectionListener() {
+            public void valueChanged(javax.swing.event.TreeSelectionEvent evt) {
+                jTree2ValueChanged(evt);
+            }
+        });
+        jPanel2.add(jTree2, java.awt.BorderLayout.PAGE_START);
+
+        jPanel5.setBackground(new java.awt.Color(235, 255, 235));
+        jPanel5.setPreferredSize(new java.awt.Dimension(164, 15));
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 164, Short.MAX_VALUE)
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jPanel2.add(jPanel5, java.awt.BorderLayout.CENTER);
+
+        leftPanel.add(jPanel2);
+
+        jPanel3.setBackground(new java.awt.Color(235, 255, 235));
+        jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel6.setText("VAROR");
+        jPanel3.add(jLabel6);
+
+        leftPanel.add(jPanel3);
+
+        jPanel1.setLayout(new java.awt.BorderLayout());
 
         jScrollPane1.setBackground(new java.awt.Color(0, 153, 0));
         jScrollPane1.setBorder(null);
@@ -329,17 +418,17 @@ public class IMatView extends javax.swing.JFrame {
 
         jTree1.setBackground(new java.awt.Color(235, 255, 236));
         jTree1.setFont(new java.awt.Font("Segoe UI Semibold", 0, 14)); // NOI18N
-        javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("root");
-        javax.swing.tree.DefaultMutableTreeNode treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Visa Alla");
+        treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("root");
+        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Visa Alla");
         treeNode1.add(treeNode2);
         treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Grönsaker");
-        javax.swing.tree.DefaultMutableTreeNode treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Rotfrukter");
+        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Rotfrukter");
         treeNode2.add(treeNode3);
         treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Kål");
         treeNode2.add(treeNode3);
         treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Grönsaksfrukter");
         treeNode2.add(treeNode3);
-        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Ärtor, Linser & Bönor");
+        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Ärtväxter");
         treeNode2.add(treeNode3);
         treeNode1.add(treeNode2);
         treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Kött");
@@ -423,58 +512,11 @@ public class IMatView extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(jTree1);
 
-        jScrollPane3.setBorder(null);
+        jPanel1.add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
-        jTree2.setRootVisible(false);
-        jTree2.setBackground(new java.awt.Color(236, 255, 235));
-        treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("root");
-        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Favoriter");
-        treeNode1.add(treeNode2);
-        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Inköpslistor");
-        treeNode1.add(treeNode2);
-        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Färdiga Kassar");
-        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Recept1");
-        treeNode2.add(treeNode3);
-        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Recept2");
-        treeNode2.add(treeNode3);
-        treeNode3 = new javax.swing.tree.DefaultMutableTreeNode("Recept3");
-        treeNode2.add(treeNode3);
-        treeNode1.add(treeNode2);
-        treeNode2 = new javax.swing.tree.DefaultMutableTreeNode("Dagens Erbjudande");
-        treeNode1.add(treeNode2);
-        jTree2.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
-        jTree2.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mousePressed(java.awt.event.MouseEvent evt) {
-                jTree2MousePressed(evt);
-            }
-        });
-        jTree2.addTreeSelectionListener(new javax.swing.event.TreeSelectionListener() {
-            public void valueChanged(javax.swing.event.TreeSelectionEvent evt) {
-                jTree2ValueChanged(evt);
-            }
-        });
-        jScrollPane3.setViewportView(jTree2);
+        leftPanel.add(jPanel1);
 
-        javax.swing.GroupLayout leftPanelLayout = new javax.swing.GroupLayout(leftPanel);
-        leftPanel.setLayout(leftPanelLayout);
-        leftPanelLayout.setHorizontalGroup(
-            leftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
-            .addComponent(jScrollPane3)
-            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
-        );
-        leftPanelLayout.setVerticalGroup(
-            leftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, leftPanelLayout.createSequentialGroup()
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 445, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(179, Short.MAX_VALUE))
-        );
-
-        mainPanel.add(leftPanel, java.awt.BorderLayout.LINE_START);
+        mainPanel.add(leftPanel, java.awt.BorderLayout.WEST);
 
         rightPanel.setBackground(new java.awt.Color(235, 255, 235));
         rightPanel.setPreferredSize(new java.awt.Dimension(300, 550));
@@ -604,7 +646,7 @@ public class IMatView extends javax.swing.JFrame {
             titlePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(titlePanelLayout.createSequentialGroup()
                 .addComponent(TitleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 68, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         titlePanelLayout.setVerticalGroup(
             titlePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -643,7 +685,7 @@ public class IMatView extends javax.swing.JFrame {
         sortPanelLayout.setHorizontalGroup(
             sortPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(sortPanelLayout.createSequentialGroup()
-                .addContainerGap(65, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(sortLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(sortingComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -728,7 +770,7 @@ public class IMatView extends javax.swing.JFrame {
             case "Grönsaksfrukter":
                 updateVarorView(ProductCategory.VEGETABLE_FRUIT);
                 break;
-            case "Ärtor, Linser & Bönor":
+            case "Ärtväxter":
                 updateVarorView(ProductCategory.POD);
                 break;
 
@@ -1216,10 +1258,15 @@ public class IMatView extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTree jTree1;
     private javax.swing.JTree jTree2;
     private javax.swing.JPanel kundvagnPanel;
@@ -1548,10 +1595,12 @@ public class IMatView extends javax.swing.JFrame {
         
 
     public void treeImage(){
-    Icon closedIcon = new ImageIcon("src/imat/resources/bilder/erbjudande.jpg");
+    /*Icon closedIcon = new ImageIcon("src/imat/resources/bilder/erbjudande.jpg");
     DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) jTree2.getCellRenderer();
     renderer.setClosedIcon(closedIcon);
-    
+    */
+        
+    jTree2.setCellRenderer(new LocalCellRenderer(jTree2));
     jTree1.setCellRenderer(new LocalCellRenderer(jTree1));
 
     }
